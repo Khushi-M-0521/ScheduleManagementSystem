d3014a3561659f2c1c63e7f85fac4e73fbadf680
Khushi-M-0521 <khushim0521@gmail.com>
2024-03-19 20:43:48 +0530

    connected TT

--- ScheduleManagement/src/myswingobjects/ClassLab.java
+++ ScheduleManagement/src/myswingobjects/ClassLab.java
@@ -1,17 +1,45 @@
 package myswingobjects;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class ClassLab extends JPanel{
-	public ClassLab(int x,int y){
+	JPanel parent;
+	JPanel tt;
+	JFrame TT;
+	public void setClassPanel() {
+		String s[]= {"hello","hi"};
+		//System.out.println("ok");
+		parent.remove(this);
+		parent.add(new ClassPanel(s,s,s,s,0,0));
+		tt.add(parent);
+		TT.repaint();
+	}
+	
+	public ClassLab(int x,int y,JPanel parent,JPanel tt,JFrame TT){
 		this.setBounds(x,y,85,63);
 		//this.setBackground(Color.lightGray);
 		this.setLayout(null);
+		this.parent=parent;
+		this.TT=TT;
+		this.tt=tt;
 		
 		MyButton cls=new MyButton("CLASS",5,5,75,14);
+		cls.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setClassPanel();
+				
+				
+			}
+			
+		});
 		this.add(cls);
 		
 		MyButton lab=new MyButton("LAB",5,5+19,75,14);
--- ScheduleManagement/src/screens/FirstScreen.java
+++ ScheduleManagement/src/screens/FirstScreen.java
@@ -7,7 +7,8 @@ public class FirstScreen {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String[] s= {"12d","cvbhj","kjvx"};
+		new AddTT("AddTT",s,s,s,s);
 	}
 
 }
